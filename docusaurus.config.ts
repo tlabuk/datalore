9db5b4d8a44e308498502432e2b07f1814c0ef3a
tlabuk <tomek.labuk@gmail.com>
2025-02-26 07:34:37 +0100

    Add UX section

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -121,7 +121,7 @@ const config: Config = {
     },
     themeConfig: {
       colorMode: {
-        defaultMode: "light",
+        defaultMode: "dark",
         disableSwitch: false,
         respectPrefersColorScheme: false,
       },
